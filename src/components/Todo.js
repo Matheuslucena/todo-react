3316cc61d60a6b0c76d058ac3b130e92af822b7c
matheusmarques <matheuslucena@outlook.com>
2021-03-02 15:30:25 -0700

    Using react hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,55 +1,37 @@
-import React, { Component } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect, useRef } from "react";
+import { useLocalStorage } from "react-use";
 import { v4 as uuidv4 } from "uuid";
 import TodoList from "./TodoList";
 
-export default class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-      todoList: [],
-      filter: "all",
-      filters: [
-        { text: "All", val: "all", selected: true },
-        { text: "Done", val: "done", selected: false },
-        { text: "Undone", val: "undone", selected: false },
-      ],
-    };
+export default function Todo(props) {
+  const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [todoFiltered, setTodoFiltered] = useState([]);
+  const [filter, setFilter] = useState("all");
+  const [filters, setFilters] = useState([
+    { text: "All", val: "all", selected: true },
+    { text: "Done", val: "done", selected: false },
+    { text: "Undone", val: "undone", selected: false },
+  ]);
+  const [todoListStorage, setTodoListStorage] = useLocalStorage(
+    "todo-list",
+    "[]"
+  );
+  const inputEl = useRef(null);
 
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleTaskDone = this.handleTaskDone.bind(this);
-  }
+  useEffect(() => {
+    setTodoList(todoListStorage);
+    inputEl.current.focus();
+  }, []);
 
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text == "") return;
+  useEffect(() => {
+    setTodoListStorage(todoList);
+  }, [todoList]);
 
-    const newList = this.state.todoList.concat({
-      id: uuidv4(),
-      text: this.state.text,
-      done: false,
-    });
-    this.setState({ text: "", todoList: newList });
-  }
-
-  handleTaskDone(task) {
-    task.done = !task.done;
-    const idx = this.state.todoList.indexOf(task);
-    const newList = [...this.state.todoList];
-    newList[idx] = task;
-    this.setState(newList);
-  }
-
-  handleInput(e) {
-    this.setState({
-      text: e.target.value,
-    });
-  }
-
-  todoListFilter() {
-    return this.state.todoList.filter((task) => {
-      switch (this.state.filter) {
+  useEffect(() => {
+    const newList = todoList.filter((task) => {
+      switch (filter) {
         case "done":
           return task.done;
         case "undone":
@@ -58,72 +40,84 @@ export default class Todo extends Component {
           return true;
       }
     });
+    setTodoFiltered(newList);
+  }, [todoList, filter]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (text == "") return;
+
+    const newList = todoList.concat({
+      id: uuidv4(),
+      text: text,
+      done: false,
+    });
+    setText("");
+    setTodoList(newList);
   }
 
-  handleClickFilter(filter) {
-    const filters = [...this.state.filters];
-    const newFilters = filters.map((f) => {
+  function handleTaskDone(task) {
+    task.done = !task.done;
+    const idx = todoList.indexOf(task);
+    const newList = [...todoList];
+    newList[idx] = task;
+    setTodoList(newList);
+  }
+
+  function handleClickFilter(filter) {
+    const filtersClone = [...filters];
+    const newFilters = filtersClone.map((f) => {
       f.selected = false;
       if (filter.text == f.text) {
         f.selected = true;
-        this.setState({ filter: filter.val });
+        setFilter(filter.val);
       }
       return f;
     });
-    this.setState({ filters: newFilters });
+    setFilters(newFilters);
   }
 
-  componentDidMount() {
-    let todoListSaved = localStorage.getItem("todo-list") || "[]";
-    this.setState({ todoList: JSON.parse(todoListSaved) });
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem("todo-list", JSON.stringify(this.state.todoList));
-  }
-
-  render() {
-    return (
-      <div className="mx-auto grid grid-flow-col sm:w-full lg:w-1/2 grid-cols-3 shadow-md rounded-lg border border-indigo-200">
-        <div className="bg-indigo-50 rounded-tl-lg rounded-bl-lg">
-          <div className="py-4">
-            <ul className="text-gray-800 text-sm">
-              {this.state.filters.map((filter) => (
-                <li
-                  key={filter.val}
-                  className={`px-4 py-1 hover:bg-indigo-200 cursor-pointer ${
-                    filter.selected ? "bg-indigo-200" : ""
-                  }`}
-                  onClick={this.handleClickFilter.bind(this, filter)}
-                >
-                  {filter.text}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="col-span-2 h-72">
-          <div className="p-4 rounded-tr-lg">
-            <form onSubmit={this.handleSubmit}>
-              <input
-                name="text"
-                type="text"
-                className="w-full border h-8 rounded px-2 focus:ring-indigo-400 focus:border-indigo-400 border-gray-300"
-                placeholder="Add new task"
-                value={this.state.text}
-                onChange={this.handleInput}
-                autoComplete="off"
-              />
-            </form>
-          </div>
-          <div className="mb-4">
-            <TodoList
-              onTaskDone={this.handleTaskDone}
-              list={this.todoListFilter.bind(this)()}
-            />
-          </div>
+  return (
+    <div className="mx-auto grid grid-flow-col sm:w-full lg:w-1/2 grid-cols-3 shadow-md rounded-lg border border-indigo-200">
+      <div className="bg-indigo-50 rounded-tl-lg rounded-bl-lg">
+        <div className="py-4">
+          <ul className="text-gray-800 text-sm">
+            {filters.map((filter) => (
+              <li
+                key={filter.val}
+                className={`px-4 py-1 hover:bg-indigo-200 cursor-pointer ${
+                  filter.selected ? "bg-indigo-200" : ""
+                }`}
+                onClick={() => handleClickFilter(filter)}
+              >
+                {filter.text}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-    );
-  }
+      <div className="col-span-2 h-72">
+        <div className="p-4 rounded-tr-lg">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              name="text"
+              type="text"
+              className="w-full border h-8 rounded px-2 focus:ring-indigo-400 focus:border-indigo-400 border-gray-300"
+              placeholder="Add new task"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              autoComplete="off"
+              ref={inputEl}
+            />
+          </form>
+        </div>
+        <div className="mb-4">
+          <TodoList
+            onTaskDone={(task) => handleTaskDone(task)}
+            list={todoFiltered}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,40 +1,26 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from "react";
+import React from "react";
 import { CheckCircleOutline } from "heroicons-react";
 
-export default class TodoList extends Component {
-  constructor(props) {
-    super(props);
+export default function TodoList(props) {
+  function handleTaskDone(task) {
+    props.onTaskDone(task);
   }
 
-  handleTaskDone(task) {
-    this.props.onTaskDone(task);
-  }
-
-  taskClass(done) {
-    let className = "px-3 py-1 flex items-center cursor-pointer ";
-    if (done) {
-      className += "text-gray-400";
-    } else {
-      className += "text-gray-800";
-    }
-    return className;
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.props.list.map((task) => (
-          <li
-            onClick={this.handleTaskDone.bind(this, task)}
-            key={task.id}
-            className={this.taskClass.bind(this, task.done)()}
-          >
-            {task.done && <CheckCircleOutline />}
-            <span className="ml-1">{task.text}</span>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {props.list.map((task) => (
+        <li
+          onClick={() => handleTaskDone(task)}
+          key={task.id}
+          className={`px-3 py-1 flex items-center cursor-pointer ${
+            task.done ? "text-gray-400 line-through" : "text-gray-800"
+          }`}
+        >
+          {task.done && <CheckCircleOutline />}
+          <span className="ml-1">{task.text}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
